60f43438b8267cd29fe3cdb17aa2b4341c073655
Chris Olsen <olsen.chris@gmail.com>
2022-12-21 13:52:31 -0700

    perform upgrade for webpack 5

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,8 @@ module.exports = {
   stories: [],
   addons: ['@storybook/addon-essentials'],
   typescript: {
-    reactDocgen: 'react-docgen-typescript',
-  }  
+    reactDocgen: 'react-docgen-typescript'
+  }
   // uncomment the property below if you want to apply some webpack config globally
   // webpackFinal: async (config, { configType }) => {
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
@@ -11,4 +11,12 @@ module.exports = {
   //   // Return the altered config
   //   return config;
   // },
+  ,
+
+  core: {
+    builder: '@storybook/builder-webpack5'
+  },
+  docs: {
+    docsPage: 'automatic'
+  }
 };
